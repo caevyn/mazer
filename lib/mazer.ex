69a4cc8bac2b23035ebea03d65e3ef88f8222d76
Matt Murphy <caevyn@gmail.com>
2015-01-21 23:02:48 +1100

    todo: fix the ugly guard on neighbour detection. works, just fugly

--- lib/mazer.ex
+++ lib/mazer.ex
@@ -4,5 +4,22 @@ defmodule Mazer do
     0..height-1 
       |> Enum.map(&(Enum.map(0..width-1, fn x -> %{x: x, y: &1, n: true, e: true, s: true, w: true, visited: false, in_path: false} end))) 
       |> Enum.concat
-  end  
-end
+  end
+
+  def pick_random_cell(cells) do
+    seed_random
+    cells |> Enum.shuffle |> Enum.at(0)
+  end
+
+  def get_unvisited_neighbours(maze, cell) do
+    maze |> Enum.filter(fn c -> is_unvisited_neighbour(cell, c) end)
+  end
+
+  def is_unvisited_neighbour(%{:x => cx, :y => cy }, %{:visited => false, :x => x, :y => y}) when (cy == y and ((x == cx-1) or (x == cx+1))) or (cx == x and ((y == cy + 1) or (y == cy-1))), do: true
+  def is_unvisited_neighbour(_, _), do: false
+
+  def seed_random do
+    << a :: 32, b :: 32, c :: 32 >> = :crypto.rand_bytes(12)
+    :random.seed(a,b,c)
+  end
+end 
--- test/mazer_test.exs
+++ test/mazer_test.exs
@@ -7,4 +7,34 @@ defmodule MazerTest do
     assert maze |> Enum.count(&(&1[:x] == 1)) == 5
     assert maze |> Enum.count(&(&1[:y] == 1)) == 4
   end
+
+
+  test "Get a random cell returns a cell" do
+    maze = Mazer.init_maze(4,5)
+    1..5 |> Enum.each(fn _ -> 
+      cell = Mazer.pick_random_cell(maze)
+      assert cell[:x] < 4
+      assert cell[:y] < 5
+    end)
+  end
+
+  test "get_unvisited_neighbours returns 2 neighbours in new maze in top corner" do
+    maze = Mazer.init_maze(4,5)
+    neighbours = Mazer.get_unvisited_neighbours(maze, Enum.at(maze, 0))
+    assert neighbours |> Enum.count == 2
+  end
+
+  test "get_unvisited_neighbours returns 3 neighbours in new maze on top edge when not a corner" do
+    maze = Mazer.init_maze(4,5)
+    neighbours = Mazer.get_unvisited_neighbours(maze, Enum.at(maze, 1)) 
+    assert neighbours |> Enum.count == 3
+  end
+
+  test "get_unvisited_neighbours returns 4 neighbours in new maze when not on edge or corner" do
+    maze = Mazer.init_maze(4,5)
+    neighbours = Mazer.get_unvisited_neighbours(maze, Enum.at(maze, 6))
+    assert neighbours |> Enum.count == 4
+  end
+
+
 end
