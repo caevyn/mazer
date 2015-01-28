7698adee1a3cb9afed413e27fa79c949ebdabab5
Matt Murphy <caevyn@gmail.com>
2015-01-28 23:16:21 +1100

    broken... todo: figure out rendering ascii

--- lib/mazer.ex
+++ lib/mazer.ex
@@ -1,9 +1,79 @@
 defmodule Mazer do
-  def init_maze(width, height) do
+  def init_cells(width, height) do
     #0..height-1 |> Enum.map(fn y -> Enum.map(0..width-1, fn x -> {x,y} end)  end) |> Enum.concat
-    0..height-1 
-      |> Enum.map(&(Enum.map(0..width-1, fn x -> %{x: x, y: &1, n: true, e: true, s: true, w: true, visited: false, in_path: false} end))) 
-      |> Enum.concat
+    for x <- 0..width-1, y <- 0..height-1, do: %{x: x, y: y, visited: false}    
+  end
+
+  def init_walls(width, height) do
+    vert = for x <- 0..width, y <- 0..height, do: %{x: x, y: y, orientation: :vertical, open: false }  
+    horz = for x <- 0..width, y <- 0..height, do: %{x: x, y: y, orientation: :horizontal, open: false }  
+    horz ++ vert
+  end
+
+  def generate_maze(width, height) do
+    cells = init_cells(width, height)
+    walls = init_walls(width, height)
+    cell = cells |> pick_random_cell
+    visit(cell, cells, walls)
+  end
+  
+  def draw_ascii(walls) do    
+    {horizontal, vertical} = walls |> Enum.split_while(&(&1.orientation == :horizontal)) 
+    h_rows = horizontal |> Enum.chunk_by(&(&1.x))
+    v_rows = vertical |> Enum.chunk_by(&(&1.x))
+    #rows = Enum.zip(h_rows, v_rows) |> Enum.reduce(fn(acc, x) -> Enum.concat(acc, Tuple.to_list(x)) end)
+    rows = for h <- h_rows, v<- v_rows, do: [h,v]
+    IO.puts(inspect(rows))
+    ascii_maze = rows |> Enum.reduce(fn(x, acc) -> acc <> print_row(x) <> "\n" end)
+    IO.puts(ascii_maze)
+  end
+
+  def print_row(row) do
+    row |> Enum.reduce(fn(x, acc) -> acc <> print_cell(x) end)
+  end
+
+  def print_cell(%{:orientation => :vertical, :open => true}),    do: "|   "
+  def print_cell(%{:orientation => :vertical, :open => false}),   do: "    "
+  def print_cell(%{:orientation => :horizontal, :open => true}),  do: "+   "
+  def print_cell(%{:orientation => :horizontal, :open => false}), do: "+---"
+  
+  def visit(cell, cells, walls) when cell != nil do
+    seed_random
+    cells = cells |> replace_list_item(cell, %{cell | visited: true})
+    next_cell = get_unvisited_neighbours(cells, cell) |> Enum.shuffle |> Enum.at(0)
+    if next_cell != nil do
+      walls = walls |> remove_wall_between(cell, next_cell)
+    end
+    visit(next_cell, cells, walls)
+  end
+
+  def visit(cell, cells, walls) do
+    #backtrack?
+    IO.puts('all visited?')
+    %{cells: cells,walls: walls}
+  end
+
+  def remove_wall_between(walls, c1 = %{x: x1, y: y1}, c2 = %{x: x2, y: y2}) when x1 == x2 do
+    wall = %{x: x1, y: min(y1, y2), orientation: :horizontal, open: false}
+    walls |> open_wall(wall)
+    #walls |> replace_list_item(edge, %{edge | open => true}
+    	#%{x: x1, y: min(y1, y2), orientation: :horizontal, open: false},
+        #%{x: x1, y: min(y1, y2), orientation: :horizontal, open: true}
+    #)
+  end
+
+  def remove_wall_between(walls, c1 = %{x: x1, y: y1}, c2 = %{x: x2, y: y2}) when y1 == y2 do
+    wall = %{x: min(x1, x2), y: y1, orientation: :vertical, open: false}
+    walls |> open_wall(wall)
+  end
+
+  def open_wall(walls, wall) do
+    walls |> replace_list_item(wall, %{wall | :open => true})
+  end
+
+  def replace_list_item(items, old_item, new_item) do
+    index = items |> Enum.find_index(&(Map.equal?(old_item, &1)))
+    items |> Enum.to_list |> List.replace_at(index, new_item)
   end
 
   def pick_random_cell(cells) do
@@ -12,7 +82,7 @@ defmodule Mazer do
   end
 
   def get_unvisited_neighbours(maze, cell) do
-    maze |> Enum.filter(fn c -> is_unvisited_neighbour(cell, c) end)
+    maze |> Enum.filter(&(is_unvisited_neighbour(cell, &1)))
   end
 
   defp is_unvisited_neighbour(%{:x => x, :y => cy }, %{:visited => false, :x => x, :y => y}) when y == cy + 1, do: true
--- test/mazer_test.exs
+++ test/mazer_test.exs
@@ -2,7 +2,7 @@ defmodule MazerTest do
   use ExUnit.Case
 
   test "4 by 5 maze has 4 columns and 5 rows" do
-    maze = Mazer.init_maze(4,5)
+    maze = Mazer.init_cells(4,5)
     assert maze |> Enum.count == 20
     assert maze |> Enum.count(&(&1[:x] == 1)) == 5
     assert maze |> Enum.count(&(&1[:y] == 1)) == 4
@@ -10,7 +10,7 @@ defmodule MazerTest do
 
 
   test "Get a random cell returns a cell" do
-    maze = Mazer.init_maze(4,5)
+    maze = Mazer.init_cells(4,5)
     1..5 |> Enum.each(fn _ -> 
       cell = Mazer.pick_random_cell(maze)
       assert cell[:x] < 4
@@ -19,21 +19,27 @@ defmodule MazerTest do
   end
 
   test "get_unvisited_neighbours returns 2 neighbours in new maze in top corner" do
-    maze = Mazer.init_maze(4,5)
+    maze = Mazer.init_cells(4,5)
     neighbours = Mazer.get_unvisited_neighbours(maze, Enum.at(maze, 0))
     assert neighbours |> Enum.count == 2
   end
 
   test "get_unvisited_neighbours returns 3 neighbours in new maze on top edge when not a corner" do
-    maze = Mazer.init_maze(4,5)
+    maze = Mazer.init_cells(4,5)
     neighbours = Mazer.get_unvisited_neighbours(maze, Enum.at(maze, 1)) 
     assert neighbours |> Enum.count == 3
   end
 
   test "get_unvisited_neighbours returns 4 neighbours in new maze when not on edge or corner" do
-    maze = Mazer.init_maze(4,5)
+    maze = Mazer.init_cells(4,5)
     neighbours = Mazer.get_unvisited_neighbours(maze, Enum.at(maze, 6))
     assert neighbours |> Enum.count == 4
+  end
+
+  test "Generated maze has all cells visited" do
+    maze = Mazer.generate_maze(3,3)
+    maze.cells |> Enum.all?(&(&1.visited))
+    Mazer.draw_ascii(maze.walls)
   end
 
 
